018594290ffbe4c5030c7526378375b88b86e9f4
Johan Grobler <liquidsquid1@gmail.com>
2017-07-25 01:12:16 +0200

    Add files via upload

---
+++ emacs-custom.el
@@ -0,0 +1,43 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(asm-comment-char 64)
+ '(company-idle-delay 0.25)
+ '(custom-enabled-themes (quote (misterioso)))
+ '(custom-safe-themes
+   (quote
+    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(minimap-mode t)
+ '(minimap-width-fraction 0.05)
+ '(minimap-window-location (quote right))
+ '(package-selected-packages
+   (quote
+    (concurrent ctable dash deferred epc jedi-core powerline python-environment rich-minority company-jedi yasnippet company-go go-mode org modern-cpp-font-lock smart-tabs-mode smart-mode-line-powerline-theme anzu smart-mode-line drag-stuff company-irony irony cmake-ide whitespace-cleanup-mode indent-guide rainbow-delimiters rainbow-mode smartparens company company-c-headers nyan-mode minimap sr-speedbar auto-complete)))
+ '(sr-speedbar-right-side nil))
+ '(sr-speedbar-right-side nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-scrollbar-bg ((t (:background "gold"))))
+ '(company-template-field ((t (:background "slate blue" :foreground "black"))))
+ '(company-tooltip ((t (:background "dark slate gray" :foreground "black"))))
+ '(company-tooltip-annotation ((t (:foreground "tomato"))))
+ '(company-tooltip-common ((t (:foreground "steel blue"))))
+ '(company-tooltip-selection ((t (:background "midnight blue"))))
+ '(indent-guide-face ((t (:foreground "gray16" :slant normal))))
+ '(minimap-active-region-background ((t (:background "dark slate gray"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orchid"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "dark cyan"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "lawn green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "gold"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "orange red"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "dim gray"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "black")))))
--- init.el
+++ init.el
@@ -1,48 +1,54 @@
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(minimap-active-region-background ((t (:background "dark slate gray"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "dark orchid"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "dark cyan"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "lawn green"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "gold"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "orange red"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "red"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "dim gray"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "black")))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(minimap-mode t)
- '(minimap-width-fraction 0.05)
- '(minimap-window-location (quote right))
- '(package-selected-packages
-   (quote
-	(drag-stuff company-irony irony cmake-ide whitespace-cleanup-mode indent-guide rainbow-delimiters rainbow-mode yasnippet smartparens company company-c-headers nyan-mode minimap sr-speedbar auto-complete))))
- '(sr-speedbar-right-side nil)
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file)
+
+(setq inhibit-splash-screen t)
 
 ;; Own customiziations.
-(defun my-insert-tab-char ()
-  "Insert a tab char. (ASCII 9, \t)"
-  (interactive)
-  (insert "\t"))
-(global-set-key (kbd "TAB") 'my-insert-tab-char)
-(setq-default tab-width 4
-			  c-basic-offset 4
+(c-add-style "my-style"
+			 '("stroustrup"
+			   (c-basic-offset . 4)
+			   (tab-width . 4)
+			   (c-offsets-alist . ((case-label . +)
+					       (inclass . ++)
+					       (access-label . -)))))
+
+(setq c-default-style "my-style")
+(defun my-c-mode-hook ()
+  ;empty for now.
+)
+;(add-hook 'c-mode-hook 'my-c-mode-hook)
+;(add-hook 'c++-mode-hook 'my-c-mode-hook)
+
+;(global-set-key (kbd "<f5>") (lambda ()
+;                               (interactive)
+;                               (setq-local compilation-read-command nil)
+;                               (call-interactively 'compile)))
+
+(setq-default ;tab-width 4
+;			  c-basic-offset 4
 			  indent-tabs-mode t)
 
 (global-linum-mode t)
+(column-number-mode t)
 
 (tool-bar-mode -1)
 
 (add-hook 'c-mode-common-hook
-	  (lambda () (subword-mode 1)))
+		  (lambda () (subword-mode 1)))
+
+(setq python-shell-interpreter "python3"
+	  python-shell-interpreter-args "-i")
+;; Readline bug fix
+(with-eval-after-load 'python
+  (defun python-shell-completion-native-try ()
+    "Return non-nil if can trigger native completion."
+    (let ((python-shell-completion-native-enable t)
+          (python-shell-completion-native-output-timeout
+           python-shell-completion-native-try-output-timeout))
+      (python-shell-completion-native-get-completions
+       (get-buffer-process (current-buffer))
+       nil "_"))))
+;; End bug fix
 ;; End personal customizations.
 
 ;; Package related.
@@ -52,16 +58,18 @@
 
 ;; Speedbar
 (require 'sr-speedbar)
+(global-set-key (kbd "C-c `") 'sr-speedbar-toggle)
 
 ;; Package: smartparens
+(require 'smartparens)
 (require 'smartparens-config)
 (show-smartparens-global-mode +1)
 (smartparens-global-mode 1)
 
 ;; When you press RET, the curly braces automatically
 ;; add another newline.
-(sp-with-modes '(c-mode c++-mode)
-  (sp-local-pair "{" nil :post-handlers '(("\t|\n[i]" "RET")))
+(sp-with-modes '(c-mode c++-mode cc-mode go-mode, ld-script-mode)
+  (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET")))
   (sp-local-pair "/*" "*/" :post-handlers '((" | " "SPC")
                                             ("* ||\n[i]" "RET"))))
 
@@ -71,13 +79,31 @@
 (global-semanticdb-minor-mode 1)
 (global-semantic-idle-scheduler-mode 1)
 (semantic-mode 1)
+(global-semantic-decoration-mode 1)
+(semantic-add-system-include "/usr/lib/avr/include" 'c-mode)
+(semantic-add-system-include "/usr/msp430/include" 'c-mode)
+(semantic-add-system-include "/usr/lib/avr/include" 'c++-mode)
+(semantic-add-system-include "/usr/msp430/include" 'c++mode)
+
 
 ;; Package: company
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
-(require 'company-c-headers)
-(add-to-list 'company-backends 'company-c-headers)
-(add-to-list 'company-c-headers-path-system "/usr/include/c++/6")
+(global-set-key (kbd "M-[") 'company-complete)
+(defun my/c-mode-hook ()
+  (require 'company-c-headers)
+  (add-to-list 'company-backends 'company-c-headers)
+  (add-to-list 'company-c-headers-path-system "/usr/include/c++/6")
+  (add-to-list 'company-c-headers-path-system "/usr/lib/avr/include")
+  (add-to-list 'company-c-headers-path-system "/usr/msp430/include"))
+(add-hook 'c-mode-common-hook 'my/c-mode-hook)
+(defun my/go-mode-hook ()
+  (require 'company-go)
+  (add-to-list 'company-backends 'company-go))
+(add-hook 'go-mode-hook 'my/go-mode-hook)
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+(add-hook 'python-mode-hook 'my/python-mode-hook)
 
 ;; Package: rainbow-delimiters
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
@@ -97,6 +123,10 @@
 ;; Package: WhiteSpace
 (require 'whitespace)
 (global-whitespace-mode)
+(setq whitespace-style '(face tabs spaces trailing lines-tail space-before-tab
+								newline indentation empty space-after-tab
+								space-mark tab-mark newline-mark))
+;;(setq whitespace-style '(face empty tabs lines-tail trailing))
   (setq whitespace-display-mappings
         '(
           (space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
@@ -116,6 +146,7 @@
 
 ;; Package: minimap
 (minimap-mode)
+(add-hook 'minimap-sb-mode-hook (lambda () (setq mode-line-format nil)))
 
 ;; Package: yasnippet
 (add-to-list 'load-path
@@ -126,3 +157,14 @@
 ;; Package: drag-stuff
 (drag-stuff-global-mode 1)
 (drag-stuff-define-keys)
+
+;; Package: smart-mode-line
+(require 'smart-mode-line)
+(setq sml/theme 'powerline)
+(sml/setup)
+
+;; Package: anzu
+(global-anzu-mode +1)
+
+;; Package: modern-cpp-font-lock
+(add-hook 'c++-mode #'modern-c++-font-lock-mode)
